17a98b8b7469c0dbfe29e818c14020f6628c1b90
ccb1139 <calebbergen000@gmail.com>
2021-11-24 18:33:30 -0500

    New Draft

---
+++ FitnessAndNutritionAppFinal/app/src/main/java/com/example/fitnessandnutritionappfinal/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.fitnessandnutritionappfinal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Ref to bottom nav view
+    BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new HomeFragment()).commit();
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch(item.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_fitness:
+                        fragment = new FitnessFragment();
+                        break;
+                    case R.id.nav_profile:
+                        fragment = new ProfileFragment();
+                        break;
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
+
+
+                return true;
+            }
+        });
+    }
+}
